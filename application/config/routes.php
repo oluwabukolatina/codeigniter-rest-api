f8b0d7d86edb079c6cdd1d7b08b11e505b8e4ddd
oluwabukola tina <oluwabukolatina2015@gmail.com>
2018-06-11 11:45:58 +0100

    clean up routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,7 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['post/add']['post'] = "api/postController/create";
-$route['login-user']['post'] = "api/userController/userlogin";
+$route['login']['post'] = 'api/authController/login';
+//$route['login-user']['post'] = "api/userController/userlogin";
 $route['post/update/(:any)']['post'] = 'api/postController/edit/$1';
 $route['user/update/(:any)']['put'] = 'api/userController/edit/$1';
 $route['post/(:any)']['get'] = 'api/postController/view/$1';
@@ -12,7 +13,6 @@ $route['user/delete/(:any)'] = 'api/userController/destroy/$1';
 $route['posts']['get'] = 'api/postController/index';
 $route['users']['get'] = 'api/userController/all';
 $route['register']['post'] = 'api/authController/register';
-$route['login']['post'] = 'api/authController/login';
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
